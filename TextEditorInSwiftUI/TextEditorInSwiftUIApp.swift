49d108ee581353e8d21e7360a18119767f6671f8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-25 13:36:16 +0300

    Initial Commit

---
+++ TextEditorInSwiftUI/TextEditorInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorInSwiftUIApp.swift
+//  TextEditorInSwiftUI
+//
+//  Created by ramil on 25.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
